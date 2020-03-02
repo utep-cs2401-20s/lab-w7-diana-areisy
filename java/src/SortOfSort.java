a2220ccad7a0e46edc1761bad1065b8672b978c3
diana-areisy <diareisy@gmail.com>
2020-03-02 14:47:50 -0700

    Started SortOfSort but not finished.

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,2 +1,16 @@
 public class SortOfSort {
+    public static void sortOfSort(int[] nums) {
+        int greatestNum = 0;
+        int k = 0;
+        for (int i = 0; i < nums.length - k ; i++) {
+            if (i + 1 < nums.length) {
+                if (nums[i] > nums[i + 1]) {
+                    greatestNum = nums[i];
+                    nums[nums.length - 1] = greatestNum;
+                }
+            }
+        }
+    }
 }
+
+
