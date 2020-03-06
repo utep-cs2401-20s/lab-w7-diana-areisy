1bb8cf21c3459f8bc1e91592284ba1a40d854509
diana-areisy <diareisy@gmail.com>
2020-03-06 16:44:11 -0700

    Finally fixed my method, created a new method to find the index of the largest number and finished test cases.

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,26 +1,54 @@
 public class SortOfSort {
-    public static void sortOfSort(int[] nums) {
-        int max = 0;
-        int k = 0;
-        int temp;
-        for (int i = 0; i < nums.length - k; i++) {
-                if (i + 1 < nums.length) {
-                    if (nums[i] > nums[i + 1]) {
-                        temp = nums[i + 1];
-                        nums[i + 1] = nums[i];
-                        nums[i] = temp;
-                        k++;
-                    } else if (nums[i + 1] > nums[i+2] && nums[i+2] < nums.length) {
-                        temp = nums[i + 2];
-                        nums[i + 2] = nums[i+1];
-                        nums[i+1] = temp;
-                        k++;
+    public void sortOfSort(int[] nums) {
 
+        int runTimes = 0; //The runTimes counts the times the method has
+                          // passed to decide where the number will be sorted
+        int start = 0; // The start integer increases every time a number is sorted in the front of the array so it wont keep sorting
+                       // that same number.
+        int end = nums.length - 1; // The end integer decreases every time a number has
+                                   // been sorted at the end of the array so it doesnt sort that number again.
 
-                    }
-                }
+        while (start != end) {
+            int maxIndex= maxIndex(nums,start, end );
+
+            //This if statement makes the integers switch from being sorted in the back,
+            //to being sorted int the front.
+            //*Ivan Arenivas helped me understand how to make the sorting
+            // go from the back to the front by using modulus.
+            if (runTimes % 4 < 2) {
+                int temp = nums[end];
+                nums[end] = nums[maxIndex];
+                nums[maxIndex] = temp;
+                end--;
+                runTimes++;
+            } else {
+                int temp = nums[start];
+                nums[start] = nums[maxIndex];
+                nums[maxIndex] = temp;
+                start++;
+                runTimes++;
             }
         }
+        //This for loop prints the final sorted array.
+        //This is just for me to see how the array resulted
+        for (int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
     }
+    //Helper method that finds the index of the largest number
+    //in the unsorted part of the array.
+    //I made a helper method because when I included this in the sortOfSort,
+    // it kept only running for certain integers and this made it cleaner and easier to understand.
+    public int maxIndex(int[] nums, int start, int end) {
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
 
 
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -4,11 +4,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SortOfSortTester {
 
+    //First test is to make sure the method is working.
     @Test
-    public void sortOfSort(){
+    public void sortOfSort1(){
 
         int[] A = {3, 1, 4, 5, 2};
-        int[] B = {1, 2, 3, 4, 5};
+        int[] B = {3, 2, 1, 4, 5};
+        SortOfSort sos = new SortOfSort();
+        sos.sortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+
+    //Second test is to make sure the method
+    // works with repeated numbers.
+    @Test
+    public void sortOfSort2(){
+
+        int[] A = {5, 2, 1, 5, 1};
+        int[] B = {2, 1, 1, 5, 5};
+        SortOfSort sos = new SortOfSort();
+        sos.sortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+
+    // Third test checks that method works
+    // with negative numbers.
+    @Test
+    public void sortOfSort3(){
+
+        int[] A = {0, 1, 35, 5, -30};
+        int[] B = {1, 0, -30, 5, 35};
+        SortOfSort sos = new SortOfSort();
+        sos.sortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+    //Fourth test checks that method works with
+    // only negative numbers.
+    @Test
+    public void sortOfSort4(){
+
+        int[] A = {-1, -2,- 3, -4, -5, -6, -7, -8, -9, -10};
+        int[] B = {-3, -4, -7, -8, -10, -9, -6, -5, -2, -1};
+        SortOfSort sos = new SortOfSort();
+        sos.sortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+    //Fifth test checks that the method
+    // works when the array has one integer.(Best case)
+    @Test
+    public void sortOfSort5(){
+
+        int[] A = {1};
+        int[] B = {1};
         SortOfSort sos = new SortOfSort();
         sos.sortOfSort(A);
         assertArrayEquals(B, A);
